caba6d55887868628d0ca64584e8c36dc9e1fbc6
Gabriele Rizzo <gabrielerizzo.pers@gmail.com>
2026-08-07 21:08:41 +0200

    Initial Commit

---
+++ Nocturne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nocturne
+//
+//  Created by Gabriele Rizzo on 07/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nocturne/NocturneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NocturneApp.swift
+//  Nocturne
+//
+//  Created by Gabriele Rizzo on 07/08/26.
+//
+
+import SwiftUI
+
+@main
+struct NocturneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
